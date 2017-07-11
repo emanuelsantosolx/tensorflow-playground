94d600779cfea82d3e7ae9af5a0b45442006f2e3
Emanuel Santos <emanuel.santos@olx.com>
2017-07-11 11:08:36 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+hello = tf.constant('This is a basic TensorFlow program!')
+sess = tf.Session()
+print(sess.run(hello))
+
+a = tf.constant(2)
+b = tf.constant(3)
+
+with tf.Session() as sess:
+    print("a=2, b=3")
+    print("Addition with constants: %i" % sess.run(a+b))
+    print("Multiplication with constants: %i" % sess.run(a*b))
+
+a = tf.placeholder(tf.int16)
+b = tf.placeholder(tf.int16)
+
+add = tf.add(a, b)
+mul = tf.multiply(a, b)
+
+with tf.Session() as sess:
+    # Run every operation with variable input
+    print("Addition with variables: %i" % sess.run(add, feed_dict={a: 2, b: 3}))
+    print("Multiplication with variables: %i" % sess.run(mul, feed_dict={a: 2, b: 3}))
+
+matrix1 = tf.constant([[3., 3.]])
+
+matrix2 = tf.constant([[2.],[2.]])
+
+product = tf.matmul(matrix1, matrix2)
+
+with tf.Session() as sess:
+    result = sess.run(product)
+    print(result)
